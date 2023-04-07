376af5d26b1fc756ea436f45a16934de818aa4d2
Eduardo Vinagre <122939534+eduvinagre@users.noreply.github.com>
2023-04-07 14:01:59 -0300

    Added Icon for weather fetch

--- script/main.js
+++ script/main.js
@@ -1,41 +1,76 @@
 window.addEventListener('load', ()=> {
     let lon;
     let lat;
-    let temperatureInfo = document.querySelector('.temperature__description')
-    let temperatureDegree = document.querySelector('.temperature__degree')
-    let locationInfo = document.querySelector('.location__timezone')
+    
+    const apiKey = '28a3d5227fa2fd44181652d1533cc617'
+    const temperatureInfo = document.querySelector('.temperature__description')
+    const temperatureDegree = document.querySelector('.temperature__degree')
+    const locationInfo = document.querySelector('.location__timezone')
+    const iconInfo = document.querySelector('.weather__icon')
     
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(location => {
             lon = location.coords.longitude
             lat = location.coords.latitude
 
-            const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}2&key=de377140d689419fafdc6ec69f2ed890&include=minutely,alerts&lang=pt&units=M`
+            const apiNow = `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${apiKey}`
 
-            // const apiforecast = `https://api.weatherbit.io/v2.0/forecast/daily?city=&lat=${lat}&lon=${long}&key=de377140d689419fafdc6ec69f2ed890&lang=pt&units=M&days=16`
-
-            fetch(api)
+            fetch(apiNow)
             .then(response => {
                 return response.json()
             })
-            .then(weather => {
-                console.log(weather)
-                const [{app_temp, city_name, weather:{description}, weather:{code}}] = weather.data
-                
-                // //API's DOM
-                temperatureDegree.textContent = app_temp
-                temperatureInfo.textContent = description 
-                locationInfo.textContent = city_name
+            .then(meteo => {
+                console.log(meteo)
+                const {name} = meteo
+                console.log(name)
+                locationInfo.textContent = name
+                const {temp} = meteo.main
+                console.log(temp)
+                temperatureDegree.textContent = temp
+                const {description, icon} = meteo.weather[0]
+                console.log(description, icon)
+                temperatureInfo.textContent = description
+                iconInfo.textContent = icon
+                iconInfo.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`            
+
             })
+
         })
 
-        
-    }
-    // }else{
-    //     // colocar aqui opção para o usuário definir a localização necessária
-    // }
+    // } else {
+    //     if(!navigator.geolocation)
+    //     const city_input = document.querySelector(".search__box")
+    //     const search_btn = document.querySelector("#search");
+    //     const apiKey = '28a3d5227fa2fd44181652d1533cc617'
 
+
+    //     search_btn.addEventListener("submit", (e) => {
+    //         e.preventDefault()
+    //         const city = city_input.value
+        
+        
+    //         const apiSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city_input}&units=metric&lang=pt_br&appid=${apiKey}`
+
+    //     fetch(apiSearch)
+    //         .then(response => {
+    //             return response.json()
+    //         })
+    //         .then(meteo => {
+    //             console.log(meteo)
+    //             const {name} = meteo
+    //             console.log(name)
+    //             locationInfo.textContent = name
+    //             const {temp} = meteo.main
+    //             console.log(temp)
+    //             temperatureDegree.textContent = temp
+    //             const {description, icon} = meteo.weather[0]
+    //             console.log(description, icon)
+    //             temperatureInfo.textContent = description
+    //             iconInfo.textContent = icon
+    //             iconInfo.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`            
+
+    //         })
+    //     }}
+    }
 })
 
-
-// ????Como acessar um array dentro de um objeto dentro de um objeto
